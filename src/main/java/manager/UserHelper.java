3b2b19acce2ac482c63261ff716a382b3cfd0efc
TayaHatum <21taya21@gmail.com>
2021-10-27 19:22:10 +0300

    added login

--- src/main/java/manager/UserHelper.java
+++ src/main/java/manager/UserHelper.java
@@ -1,10 +1,45 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UserHelper extends HelperBase{
 
     public UserHelper(WebDriver wd) {
         super(wd);
+    }
+
+    public boolean isLoginPresent() {
+        return isElementPresent(By.xpath("//a[text()=' Log in ']"));
+    }
+
+    public void logout() {
+        click(By.xpath("//a[text()=' Logout ']"));
+    }
+
+    public void openLoginForm() {
+        click(By.xpath("//a[text()=' Log in ']"));
+    }
+
+    public void fillLoginForm(String email, String password) {
+        type(By.id("email"),email);
+        type(By.id("password"),password);
+    }
+
+    public void submitForm() {
+       // click(By.cssSelector("[type='submit']"));
+        WebElement submit= wd.findElement(By.cssSelector("[type='submit']"));
+        submit.submit();
+
+    }
+
+    public boolean isLoggedSuccess() {
+        WebDriverWait wait = new WebDriverWait(wd,10);
+        wait.until(ExpectedConditions.visibilityOf(wd.findElement(By.cssSelector(".dialog-container"))));
+
+return wd.findElement(By.cssSelector(".dialog-container h2")).getText().contains("success");
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,27 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+@BeforeMethod
+public void precondition(){
+    if(!app.getUserHelper().isLoginPresent()){
+        app.getUserHelper().logout();
+    }
+
+}
+
+
     @Test
-    public void test(){
+    public void loginSuccess(){
+    app.getUserHelper().openLoginForm();
+    app.getUserHelper().fillLoginForm("noa@gmail.com","Nnoa12345$");
+    app.getUserHelper().submitForm();
+        Assert.assertTrue(app.getUserHelper().isLoggedSuccess());
+
 
     }
 }
