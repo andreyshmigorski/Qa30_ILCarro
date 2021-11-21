99998cfd9c581ccdfd5d304bf7da30bdccf03923
TayaHatum <21taya21@gmail.com>
2021-11-21 19:27:28 +0200

    added search

--- src/main/java/manager/SearchHelper.java
+++ src/main/java/manager/SearchHelper.java
@@ -1,9 +1,59 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SearchHelper extends HelperBase{
+import java.time.LocalDate;
+
+public class SearchHelper extends HelperBase {
     public SearchHelper(WebDriver wd) {
         super(wd);
+    }
+
+    public void fillSearchForm(String city, String from, String to) {
+        fillInputCity(city);
+        selectPeriod(from, to);
+    }
+
+    private void selectPeriod(String from, String to) {
+        //   "11/25/2021","12/26/2021"
+
+        String[] dataFrom = from.split("/");  // dataFrom[1]
+        String[] dataTo = to.split("/"); // dataTo[1]
+        click(By.id("dates"));
+
+        int diffStart=0;
+        if(LocalDate.now().getMonthValue()!=Integer.parseInt(dataFrom[0])){
+            diffStart = Integer.parseInt(dataFrom[0])-LocalDate.now().getMonthValue();
+        }
+
+        int diff=0;
+        if(Integer.parseInt(dataFrom[0])!=Integer.parseInt(dataTo[0])){
+            diff = Integer.parseInt(dataTo[0])-Integer.parseInt(dataFrom[0]);
+        }
+
+        for (int i = 0; i < diffStart; i++) {
+            click(By.xpath("//button[@aria-label='Next month']"));
+        }
+        String locator3 = String.format("//div[.=' %s ']", dataFrom[1]);
+        click(By.xpath(locator3));
+
+        for (int i = 0; i < diff; i++) {
+            click(By.xpath("//button[@aria-label='Next month']"));
+        }
+
+        String locator = "//div[.=' 25 ']";
+        String locator2 = "//div[.=' " + dataFrom[1] + " ']";
+        String locator4 = String.format("//div[.=' %s ']", dataTo[1]);
+        click(By.xpath(locator4));
+
+    }
+
+
+    private void fillInputCity(String city) {
+        type(By.id("city"), city);
+        click(By.cssSelector("div.pac-item"));
+        pause(500);
+
     }
 }
--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,4 +1,16 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SearchTest extends TestBase{
+
+    @Test
+    public void searchTests(){
+        app.search().fillSearchForm("Haifa","11/25/2021","11/30/2021");
+        //app.search().clickYallaButton();
+        app.getUserHelper().submitForm();
+
+        //Assert.assertTrue(app.search().isListOfCarsAppeared());
+    }
 }
